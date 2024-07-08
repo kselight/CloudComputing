5ca95e2e64fe4affcc84ce4f26ef420de65f1c39
kselight <18469848+kselight@users.noreply.github.com>
2024-07-08 21:02:09 +0200

    13

--- cloudcomputing/src/main/java/com/kselight/cloudcomputing/helloworld/application/controller/HelloWorldController.java
+++ cloudcomputing/src/main/java/com/kselight/cloudcomputing/helloworld/application/controller/HelloWorldController.java
@@ -2,11 +2,12 @@ package com.kselight.cloudcomputing.helloworld.application.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("")
+    @GetMapping("/hello/world")
     ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello World");
     }
